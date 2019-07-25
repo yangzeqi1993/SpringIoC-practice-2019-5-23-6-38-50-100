7d12b58998c478a36e87c3c3eee0807c1813a5fd
yang <yangzq19931003@outlook.com>
2019-07-25 17:02:29 +0800

    提交作业

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,21 @@
 //TODO
+package com.example.SpringIoCpractice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        //1加载配置文件，根据创建对象
+        ApplicationContext context = new FileSystemXmlApplicationContext("hello.xml");
+        //2得到创建的对象
+        HelloWorld hello=(HelloWorld) context.getBean("helloworld");
+        hello.getHello();
+        //System.out.println(hello);;
+    }
+
+}
--- src/test/java/com/example/SpringIoCpractice/SpringIoCPracticeApplicationTests.java
+++ src/test/java/com/example/SpringIoCpractice/SpringIoCPracticeApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.SpringIoCpractice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -11,6 +13,13 @@ public class SpringIoCPracticeApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+//		//1加载配置文件，根据创建对象
+//		ApplicationContext context = new FileSystemXmlApplicationContext("hello.xml");
+//		//2得到创建的对象
+//		HelloWorld hello=(HelloWorld) context.getBean("helloworld");
+//		hello.getHello();
+//		//System.out.println(hello);
 	}
 
 }
